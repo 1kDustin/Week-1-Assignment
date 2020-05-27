7b464a5643da403d3a265be58b2a54452963c1b4
1kdustin <dustinschomburg94@gmail.com>
2020-05-27 18:37:17 -0500

    My completed assignment for week 1

---
+++ index.js
@@ -0,0 +1,30 @@
+let applePrice = 2.35;
+let bananaPrice = 2.45;
+
+let myMoney = 25;
+let wifesMoney = 30;
+
+
+let numberOfFriends = 50;
+let wifesNumberOfFriendsTwo = 100;
+
+let myAge = 25;
+let myWifesAge = 23;
+
+let firstName = 'Dustin';
+let wifesFirstName = 'Brooke';
+
+let lastName = 'Schomburg';
+let wifesLastName = 'Schomburg';
+
+let middleInitial = 'L';
+let wifesMiddleInitial = 'N';
+
+let myMoneyAfterPurchase = (myMoney - applePrice);
+console.log(myMoneyAfterPurchase + ' is how much money I have left after I bought an apple.');
+
+let numberOfFriendsPerYear = (numberOfFriends / myAge);
+console.log(numberOfFriendsPerYear + ' is the number of friends I have made per year');
+
+let fullName = (firstName + middleInitial + lastName);
+console.log(fullName);
