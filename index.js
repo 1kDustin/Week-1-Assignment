ec6bf09b32b74ac4fbaff4b12aef5a52b8e5828f
1kdustin <dustinschomburg94@gmail.com>
2020-05-31 17:25:52 -0500

    test run with -f

--- index.js
+++ index.js
@@ -28,3 +28,48 @@ console.log(numberOfFriendsPerYear + ' is the number of friends I have made per 
 
 let fullName = (firstName + middleInitial + lastName);
 console.log(fullName);
+
+let firstname = 'Brooke';
+
+if (firstname == 'Brooke') {
+    console.log('Hello, Brooke')
+} else {
+    console.log('Hey, ' + firstname);
+}
+
+let costOfMIlk = 2;
+
+if (costOfMIlk < 2) {
+    console.log('We will buy 2 gallons');
+} else if (costOfMIlk < 3) {
+    console.log('We will buy one gallon');
+} else {
+    console.log('No thanks, way too expensive!')
+}
+
+let grade = 'c';
+
+switch (grade) {
+    case 'a':
+        console.log('90-100');
+        break;
+        case 'b':
+            console.log('80-89');
+            break
+            case 'c':
+                console.log('70-79');
+                break
+                default: 
+                console.log('0-69');
+}
+
+let a = 5;
+let b = 6;
+
+if (a ==b) {
+    console.log('a is 5 and b is 6');
+} else {
+    console.log('a is not equal to be')
+} else if {
+    console.log('a is equal to b!!')
+}
